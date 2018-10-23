6e47f2ef01016efac893d484a1c88ad7942ccb4e
wojprz <wojprz@st.amu.edu.pl>
2018-10-23 22:17:32 +0200

    Add Mapper

---
+++ SerwisProduktow.Infrastructure/Mappers/AutoMapperConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AutoMapper;
+using SerwisProduktow.Domain;
+using SerwisProduktow.Domain.Entities;
+using SerwisProduktow.Infrastructure.DTO;
+
+namespace SerwisProduktow.Infrastructure.Mappers
+{
+    class AutoMapperConfig
+    {
+        public static IMapper Initialize()
+            => new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Category_Services, CategoryDto>();
+                cfg.CreateMap<Comment, CommentDto>();
+                cfg.CreateMap<Rating, RatingDto>();
+                cfg.CreateMap<Service, ServiceDto>();
+                cfg.CreateMap<User, UserDto>();
+            })
+            .CreateMapper();
+    
+    }
+}

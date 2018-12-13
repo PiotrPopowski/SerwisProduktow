290e5bf7d4428da634297e592220976469e2136e
Pplociennik <przemx.84@gmail.com>
2018-12-13 22:15:52 +0100

    Changed service model

--- Client/src/app/shared/service.model.ts
+++ Client/src/app/shared/service.model.ts
@@ -1,10 +1,15 @@
+import { Category } from "./category.model";
+import { Comment } from "./comment.model";
+import { Rating } from "./rating.model";
+import { User } from "./user.model";
+
 export class Service {
-    Category: Object;
-    Comments: Object;
+    Category: Category;
+    Comments: Comment;
     DateOfAddition: Date;
     Descryption: String;
     ID: Number;
-    Rating: Object;
+    Rating: Rating;
     Status: Number;
-    User: Object;
+    User: User;
   }

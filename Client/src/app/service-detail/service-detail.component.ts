4efd7694ae5fc795cd7e1f8d236cbb97ba006baf
Pplociennik <przemx.84@gmail.com>
2019-01-07 16:35:08 +0100

    ...

--- Client/src/app/service-detail/service-detail.component.ts
+++ Client/src/app/service-detail/service-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicesService } from '../services.service';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-service-detail',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./service-detail.component.css']
 })
 export class ServiceDetailComponent implements OnInit {
+  service;
 
-  constructor() { }
+
+  constructor(private servicesService: ServicesService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.getService();
   }
-
+  getService(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.servicesService.getService(id)
+      .subscribe(service => this.service = service);
+  }
 }

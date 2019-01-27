99d4681581aa0bb489e8d3e5f39719420d8de7b1
Piotrek <p.popowski01@gmail.com>
2019-01-27 16:24:24 +0100

    Add Rating

---
+++ Server/SerwisProduktow.Infrastructure/ViewModels/RatingModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerwisProduktow.Infrastructure.ViewModels
+{
+    public class RatingModel
+    {
+        public int Rate { get; set; }
+        public int ServiceID { get; set; }
+    }
+}
